d1cbc31fce1a7c701056a3e9595536af491c493b
Anshgarhewalmusicplayer <anshgarhewal8@gmail.com>
2020-12-31 19:41:56 +0530

    First Commit.

---
+++ app/src/main/java/com/example/music_player/utils/Song.java
@@ -0,0 +1,29 @@
+package com.example.music_player.utils;
+
+public class Song {
+    private String songName,songUrl;
+
+    public Song() {
+    }
+
+    public Song(String songName, String songUrl) {
+        this.songName = songName;
+        this.songUrl = songUrl;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSongUrl() {
+        return songUrl;
+    }
+
+    public void setSongUrl(String songUrl) {
+        this.songUrl = songUrl;
+    }
+}
